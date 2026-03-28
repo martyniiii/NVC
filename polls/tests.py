aa8998e0393bfb5a3130e17291f9ab73b04647fb
martyniiii <sztandomartyna1@gmail.com>
2026-03-28 14:49:40 +0100

    Fix model, add admin.display and tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,16 +1,9 @@
-
 import datetime
 
 from django.test import TestCase
 from django.utils import timezone
-from django.urls import reverse
 
 from .models import Question
-
-
-def create_question(question_text, days):
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
 
 
 class QuestionModelTests(TestCase):
@@ -28,25 +21,3 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
-
-
-class QuestionIndexViewTests(TestCase):
-    def test_no_questions(self):
-        response = self.client.get(reverse("polls:index"))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No polls are available.")
-        self.assertQuerySetEqual(response.context["latest_question_list"], [])
-
-    def test_past_question(self):
-        question = create_question("Past question.", days=-30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertQuerySetEqual(
-            response.context["latest_question_list"],
-            [question],
-        )
-
-    def test_future_question(self):
-        create_question("Future question.", days=30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertContains(response, "No polls are available.")
-        self.assertQuerySetEqual(response.context["latest_question_list"], [])
