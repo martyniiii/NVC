5cdbd5eceef318842deecb0dcfb3d87dd8bc4fa3
martyniiii <sztandomartyna1@gmail.com>
2026-05-14 16:53:12 +0200

    Add missing MVC folders

---
+++ Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Range(1, 100)]
+        public decimal Price { get; set; }
+    }
+}
